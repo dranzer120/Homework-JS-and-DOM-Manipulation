3e8a8625e14d5d02ae50e0c298f4316f5b3e5654
Jason Tong <jasontong818@gmail.com>
2019-07-28 00:19:32 +1000

    Final Commit

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,32 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+tableData.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+  var button = d3.select("#filter-btn");
+
+  button.on("click", function() {
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    var filteredData = tableData.filter(row => row.datetime === inputValue);
+
+    tbody.html("");
+
+    filteredData.forEach((ufoSighting) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoSighting).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+  })
